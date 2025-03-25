55438fadbee231ef1756b5f4ac84ad1529e8ed7a
Claudius Marco Andrew <claudiusma2022@gmail.com>
2025-03-24 23:53:55 -0500

    Added routes to next page of Gallery

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Homepage from "./Homepage/Homepage.js"; // Correct the path based on where the file is located
 import Navbar from "./Navbar/Navbar.js"; // Import the Navbar component
-import "./App.css"; // Global styles for the app
+import Gallery from "./Gallery/Gallery.js"; // Import the Gallery component
+import GalleryDetail from "./Gallery/GalleryDetail"; // Import the new GalleryDetail component
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Use Routes instead of Switch
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Homepage /> {/* Render the Homepage component */}
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/gallery/:id" element={<GalleryDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -2,6 +2,8 @@ import "./Gallery.css"; // Import the new CSS for gallery styling
 
 import portf1 from "./portf1.jpeg"; // Import the image from the Gallery folder
 
+import { Link } from "react-router-dom"; // Import Link for routing
+
 function Gallery() {
   return (
     /* Wrap everything in a single parent */
@@ -13,16 +15,20 @@ function Gallery() {
       </div>
       <div className="gallery">
         <div className="gallery-item">
-          <img src={portf1} alt="Image" className="gallery-image" />
-          <div className="gallery-info">
-            <h4>2/28/25</h4>
-            <h3>Eye Museum</h3>
-            <p>
-              Eye Filmmuseum is a film archive, museum, and cinema in Amsterdam
-              that presents and screens both Dutch and foreign films screened in
-              the Netherlands.
-            </p>
-          </div>
+          <Link to="/gallery/1">
+            {" "}
+            {/* Use Link to navigate to the specific gallery page */}
+            <img src={portf1} alt="Image" className="gallery-image" />
+            <div className="gallery-info">
+              <h4>2/28/25</h4>
+              <h3>Eye Museum</h3>
+              <p>
+                Eye Filmmuseum is a film archive, museum, and cinema in
+                Amsterdam that presents and screens both Dutch and foreign films
+                screened in the Netherlands.
+              </p>
+            </div>
+          </Link>
         </div>
 
         <div className="gallery-item">
